4d7aa56cd494c6b869f14b46fe604c997ce83cb8
Yining Ma <mayining0822@gmail.com>
2024-02-16 09:58:22 -0500

    Add Mobster test part

--- vignettes/TEST_Yining.r
+++ vignettes/TEST_Yining.r
@@ -1,10 +1,11 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Khanh - Macbook
 # R_workplace <- "/Users/dinhngockhanh/Library/CloudStorage/GoogleDrive-knd2127@columbia.edu/My Drive/RESEARCH AND EVERYTHING/Projects/GITHUB/SFS_CNA_deconvolution/vignettes"
-R_workplace <- "C://Users/Mayin/Documents/1GRADUATE/1. Study/2. Sprint/5398 Dinh/github_clone/SFS_CNA_deconvolution-1/vignettes"
-
-R_libPaths <- ""
-R_libPaths_extra <- "C://Users/Mayin/Documents/1GRADUATE/1. Study/2. Sprint/5398 Dinh/github_clone/SFS_CNA_deconvolution-1/R"
+# R_libPaths <- ""
 # R_libPaths_extra <- "/Users/dinhngockhanh/Library/CloudStorage/GoogleDrive-knd2127@columbia.edu/My Drive/RESEARCH AND EVERYTHING/Projects/GITHUB/SFS_CNA_deconvolution/R"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Yining - Laptop
+R_workplace <- "C:/Users/Mayin/Documents/1GRADUATE/1. Study/2. 24Spring/5398 Dinh/DATA"
+R_libPaths <- ""
+R_libPaths_extra <- "C:/Users/Mayin/Documents/1GRADUATE/1. Study/2. 24Spring/5398 Dinh/github_clone/SFS_CNA_deconvolution-1/R"
 # =======================================SET UP FOLDER PATHS & LIBRARIES
 .libPaths(R_libPaths)
 library(data.table)
@@ -19,7 +20,7 @@ files_sources <- list.files(pattern = "\\.[rR]$")
 sapply(files_sources, source)
 setwd(R_workplace)
 
-folder_workplace <- "TEST_SFS_DECONVOLUTION/"
+folder_workplace <- "03_TEST_SFS_DECONVOLUTION/"
 #---------------------------------------------------Set model parameters
 n_simulations <- 100
 
@@ -32,7 +33,7 @@ vec_time_points_s_mut <- t_end_time * c(0.6)
 vec_hierarchy_s_mut <- c(0)
 expected_end_population <- 10^6
 vec_expected_percent_select <- (1 / (n_selective_clones + 1)) * rep(1, length = (n_selective_clones + 1))
-n_sample <- 100000
+n_sample <- 10000  # CHANGE from 100000 to 10000
 range_population <- c(0.8, 1.2) * expected_end_population
 range_clonal_perc <- c(20, 100)
 # mindiff_clonal_perc <- 10
@@ -84,3 +85,94 @@ for (n_simulation in 1:n_simulations) {
     write.table(mut_table, filename, sep = " ", row.names = FALSE, col.names = FALSE)
 }
 # ===============================================================MOBSTER
+library(mobster) # Load the mobster package
+df <- data.frame() # Create an empty data frame
+for (i in 1:n_simulations) {
+    print(paste("Dealing with file: ", i))
+    # import data
+    # base_filename <- paste(folder_workplace , sep = " ", " ")"C:/Users/Mayin/Documents/1GRADUATE/1. Study/2. 24Spring/5398 Dinh/DATA/02_TEST_SFS_DECONVOLUTION/"
+    filename <- paste0(folder_workplace,"SFS_", i, ".txt")
+    txtdata <- read.table(file = filename, header = FALSE)
+
+    # data transformation
+    data <- transform(txtdata, VAF = txtdata[,2] / (txtdata[,1] + txtdata[,2]))
+    last_col <- ncol(data)
+    mob_data <- as.data.frame(data[,last_col])
+    colnames(mob_data)[1] <- "VAF"
+
+    # # data check
+    # head(mob_data,5)
+    # class(mob_data)
+    # hist(mob_data[,1])
+    # Sys.sleep(3)
+
+    # mobster
+    # mobster:::template_parameters_fast_setup() # show basic setup
+    fit <-  mobster_fit(
+    mob_data,    
+    auto_setup = "FAST"
+    )
+
+    print("Start fitting")
+    mob_model <- fit$best # best model
+    print("Fitting done")
+
+    # plot_path <- paste0("C:/Users/Mayin/Desktop/02_Mobster_Test_plot/", i, ".png")
+    # png(plot_path) # Save the plot
+    # plot(mob_model)
+    # dev.off()
+
+
+    
+     print("Start saving")
+    df[i,"id"] <- i # id
+    df[i, "Total_N"] <- mob_model$N # total acount
+
+    # tail part 
+    df[i, "Tail"] <- mob_model$fit.tail # bool: if tail exists
+    df[i, "Tail_Num"] <- mob_model$N.k[[1]] # number of tail
+    df[i, "Tail_shape"] <- mob_model$shape # shape of tail
+    df[i, "Tail_scale"] <- mob_model$scale # scale of tail
+
+    # cluster part
+    df[i, "Kbeta_cluster"] <- mob_model$Kbeta # number of clusters
+    for (k in 1:mob_model$Kbeta){
+    df[i, paste0("cl_num_", k)] <- mob_model$N.k[[k+1]] # alpha of Beta
+    df[i, paste0("a_", k)] <- mob_model$a[[k]] # alpha of Beta
+    df[i, paste0("b_", k)] <- mob_model$b[[k]] # beta of Beta
+    }
+
+    print("Saving done")
+    print("=====================================")
+
+    #print finished
+    if (i == n_simulations){
+        print("=====================================")
+        print(paste("||       All done ", n_simulations, " files!      ||"))
+        print("=====================================")
+    }
+}
+
+# Summary of the result
+print(paste("Shape of the result: ", dim(df)[1],",", dim(df)[2]))
+print(head(df, 5))
+csv_name <- paste0(folder_workplace, "Mobster_Test.csv")
+write.csv(df, file = csv_name)
+
+
+
+    # ...
+# ...
+# ...
+# ...
+# ...
+
+# ===============================================================GROUND TRUTH
+for (n_simulation in 1:n_simulations) {
+    # ...
+# ...
+# ...
+# ...
+# ...
+}
+# ===============================================================COMPARISON
