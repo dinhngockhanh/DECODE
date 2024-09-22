e61782a288f0724c8d9cfea332aa82deb275a3b8
Dinh Ngoc Khanh <knd2127@columbia.edu>
2024-09-22 16:21:54 -0400

    .

--- vignettes/OLD_DECODE_for_ICGC.r
+++ vignettes/OLD_DECODE_for_ICGC.r
@@ -60,81 +60,81 @@ sample_df <- sample_df[
     ),
 ]
 write.csv(sample_df, file = paste0(R_workplace, "/ICGC_sample_information.csv"), row.names = FALSE)
-# # ===============================================================MOBSTER
-# folder_workplace <- "ICGC-MOBSTER/"
-# dir.create(folder_workplace)
-# numCores <- detectCores()
-# cl <- makePSOCKcluster(numCores - 1)
-# if (is.null(R_libPaths) == FALSE) {
-#     R_libPaths <<- R_libPaths
-#     clusterExport(cl, varlist = c("R_libPaths"))
-#     clusterEvalQ(cl = cl, .libPaths(R_libPaths))
-# }
-# clusterExport(cl, varlist = c(
-#     "folder_workplace",
-#     "R_data",
-#     "sample_df"
-# ))
-# clusterEvalQ(cl, library(mobster))
-# df_all_mobsters <- pblapply(cl = cl, X = 1:length(sample_df$aliquot_id), FUN = function(n_sample) {
-#     sample <- sample_df$aliquot_id[n_sample]
-#     #   Import mutational data
-#     filename_2 <- paste0(R_data, "/", sample, "_1_1.csv")
-#     mutation_table <- read.csv(filename_2, sep = "\t", header = TRUE)
-#     mutation_table$Ref_count <- mutation_table$t_ref_count
-#     mutation_table$Alt_count <- mutation_table$t_alt_count
-#     if (any(is.na(mutation_table$Ref_count)) | any(is.na(mutation_table$Alt_count))) {
-#         mutation_table <- mutation_table[-which(is.na(mutation_table$Ref_count) | is.na(mutation_table$Alt_count)), ]
-#     }
-#     if (any(mutation_table$Ref_count == 0) | any(mutation_table$Alt_count == 0)) {
-#         mutation_table <- mutation_table[-which(mutation_table$Ref_count == 0 | mutation_table$Alt_count == 0), ]
-#     }
-#     #   Data transformation
-#     mutation_table$VAF <- mutation_table$Alt_count / (mutation_table$Alt_count + mutation_table$Ref_count)
-#     mob_data <- as.data.frame(mutation_table$VAF)
-#     colnames(mob_data)[1] <- "VAF"
-#     #   SFS deconvolution with MOBSTER
-#     MOBSTER_result <- mobster_fit(
-#         mob_data,
-#         maxIter = 1000, # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-#         parallel = FALSE,
-#         description = sample
-#     )
-#     #   Find best MOBSTER model
-#     png(paste0(folder_workplace, "MOBSTER_", sample, "_1_1.png"), res = 150, width = 15, height = 7.5, units = "in")
-#     print(plot(MOBSTER_result$best))
-#     dev.off()
-#     png(paste0(folder_workplace, "MOBSTER_model_selection_", sample, "_1_1.png"), res = 150, width = 15, height = 7.5, units = "in")
-#     print(plot_model_selection(MOBSTER_result))
-#     dev.off()
-#     return(MOBSTER_result)
-# })
-# stopCluster(cl)
-# mobster_df <- data.frame()
-# mobster_fits <- list()
-# for (n_sample in 1:length(sample_df$aliquot_id)) {
-#     sample <- sample_df$aliquot_id[n_sample]
-#     MOBSTER_result <- df_all_mobsters[[n_sample]]
-#     #---Save MOBSTER results
-#     mobster_fits[[n_simulation]] <- MOBSTER_result
-#     mobster_model <- MOBSTER_result$best
-#     mobster_df[n_simulation, "Sample"] <- sample
-#     mobster_df[n_simulation, "Mutation_count_in_fitting"] <- mobster_model$N
-#     mobster_df[n_simulation, "Tail"] <- mobster_model$fit.tail
-#     mobster_df[n_simulation, "Tail_power"] <- mobster_model$shape + 1
-#     mobster_df[n_simulation, "Tail_pareto_shape"] <- mobster_model$shape
-#     mobster_df[n_simulation, "Tail_pareto_scale"] <- mobster_model$scale
-#     mobster_df[n_simulation, "Tail_mutcount_observed"] <- mobster_model$N.k[[1]]
-#     mobster_df[n_simulation, "Cluster_count"] <- mobster_model$Kbeta
-#     for (k in 1:mobster_model$Kbeta) {
-#         mobster_df[n_simulation, paste0("Cluster_mutcount_observed_", k)] <- mobster_model$N.k[[k + 1]]
-#         mobster_df[n_simulation, paste0("Cluster_frequency_", k)] <- mobster_model$a[[k]] / (mobster_model$a[[k]] + mobster_model$b[[k]])
-#         mobster_df[n_simulation, paste0("Cluster_beta_a_", k)] <- mobster_model$a[[k]]
-#         mobster_df[n_simulation, paste0("Cluster_beta_b_", k)] <- mobster_model$b[[k]]
-#     }
-# }
-# write.csv(mobster_df, paste0("Parameters_ICGC_MOBSTER"), row.names = FALSE)
-# save(mobster_fits, file = paste0("ICGC_MOBSTER.rda"))
+# ===============================================================MOBSTER
+folder_workplace <- "ICGC-MOBSTER/"
+dir.create(folder_workplace)
+numCores <- detectCores()
+cl <- makePSOCKcluster(numCores - 1)
+if (is.null(R_libPaths) == FALSE) {
+    R_libPaths <<- R_libPaths
+    clusterExport(cl, varlist = c("R_libPaths"))
+    clusterEvalQ(cl = cl, .libPaths(R_libPaths))
+}
+clusterExport(cl, varlist = c(
+    "folder_workplace",
+    "R_data",
+    "sample_df"
+))
+clusterEvalQ(cl, library(mobster))
+df_all_mobsters <- pblapply(cl = cl, X = 1:length(sample_df$aliquot_id), FUN = function(n_sample) {
+    sample <- sample_df$aliquot_id[n_sample]
+    #   Import mutational data
+    filename_2 <- paste0(R_data, "/", sample, "_1_1.csv")
+    mutation_table <- read.csv(filename_2, sep = "\t", header = TRUE)
+    mutation_table$Ref_count <- mutation_table$t_ref_count
+    mutation_table$Alt_count <- mutation_table$t_alt_count
+    if (any(is.na(mutation_table$Ref_count)) | any(is.na(mutation_table$Alt_count))) {
+        mutation_table <- mutation_table[-which(is.na(mutation_table$Ref_count) | is.na(mutation_table$Alt_count)), ]
+    }
+    if (any(mutation_table$Ref_count == 0) | any(mutation_table$Alt_count == 0)) {
+        mutation_table <- mutation_table[-which(mutation_table$Ref_count == 0 | mutation_table$Alt_count == 0), ]
+    }
+    #   Data transformation
+    mutation_table$VAF <- mutation_table$Alt_count / (mutation_table$Alt_count + mutation_table$Ref_count)
+    mob_data <- as.data.frame(mutation_table$VAF)
+    colnames(mob_data)[1] <- "VAF"
+    #   SFS deconvolution with MOBSTER
+    MOBSTER_result <- mobster_fit(
+        mob_data,
+        maxIter = 1000, # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        parallel = FALSE,
+        description = sample
+    )
+    #   Find best MOBSTER model
+    png(paste0(folder_workplace, "MOBSTER_", sample, "_1_1.png"), res = 150, width = 15, height = 7.5, units = "in")
+    print(plot(MOBSTER_result$best))
+    dev.off()
+    png(paste0(folder_workplace, "MOBSTER_model_selection_", sample, "_1_1.png"), res = 150, width = 15, height = 7.5, units = "in")
+    print(plot_model_selection(MOBSTER_result))
+    dev.off()
+    return(MOBSTER_result)
+})
+stopCluster(cl)
+mobster_df <- data.frame()
+mobster_fits <- list()
+for (n_sample in 1:length(sample_df$aliquot_id)) {
+    sample <- sample_df$aliquot_id[n_sample]
+    MOBSTER_result <- df_all_mobsters[[n_sample]]
+    #---Save MOBSTER results
+    mobster_fits[[n_simulation]] <- MOBSTER_result
+    mobster_model <- MOBSTER_result$best
+    mobster_df[n_simulation, "Sample"] <- sample
+    mobster_df[n_simulation, "Mutation_count_in_fitting"] <- mobster_model$N
+    mobster_df[n_simulation, "Tail"] <- mobster_model$fit.tail
+    mobster_df[n_simulation, "Tail_power"] <- mobster_model$shape + 1
+    mobster_df[n_simulation, "Tail_pareto_shape"] <- mobster_model$shape
+    mobster_df[n_simulation, "Tail_pareto_scale"] <- mobster_model$scale
+    mobster_df[n_simulation, "Tail_mutcount_observed"] <- mobster_model$N.k[[1]]
+    mobster_df[n_simulation, "Cluster_count"] <- mobster_model$Kbeta
+    for (k in 1:mobster_model$Kbeta) {
+        mobster_df[n_simulation, paste0("Cluster_mutcount_observed_", k)] <- mobster_model$N.k[[k + 1]]
+        mobster_df[n_simulation, paste0("Cluster_frequency_", k)] <- mobster_model$a[[k]] / (mobster_model$a[[k]] + mobster_model$b[[k]])
+        mobster_df[n_simulation, paste0("Cluster_beta_a_", k)] <- mobster_model$a[[k]]
+        mobster_df[n_simulation, paste0("Cluster_beta_b_", k)] <- mobster_model$b[[k]]
+    }
+}
+write.csv(mobster_df, paste0("Parameters_ICGC_MOBSTER"), row.names = FALSE)
+save(mobster_fits, file = paste0("ICGC_MOBSTER.rda"))
 # # ================================================================DECODE
 # folder_workplace <- "ICGC-DECODE/"
 # dir.create(folder_workplace)
